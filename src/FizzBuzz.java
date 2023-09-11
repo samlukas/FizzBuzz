215399de6a42e0229cbb356528609606a21200a6
samlukas <samuelplukas@gmail.com>
2023-09-11 15:54:13 -0400

    added doFizzBuzz method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,35 +8,61 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            i = doFizzBuzz(i);
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+
+            i++;
         }
     }
 
-    private static int doFizzBuzz(int i) {
-        // Find out which numbers divide i.
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
+    public static void doFizzBuzz(int i) {
+        int n = 1;
+        while (n < i) {
 
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = n % 3 == 0;
+            boolean divisibleBy5 = n % 5 == 0;
 
-            System.out.println("Fizz Buzz");
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
 
-        } else if (divisibleBy3) {
+                System.out.println("Fizz Buzz");
 
-            System.out.println("Fizz");
+            } else if (divisibleBy3) {
 
-        } else if (divisibleBy5) {
+                System.out.println("Fizz");
 
-            System.out.println("Buzz");
+            } else if (divisibleBy5) {
 
-        } else {
+                System.out.println("Buzz");
 
-            System.out.println(i);
+            } else {
 
+                System.out.println(n);
+
+            }
+
+            n++;
         }
-
-        i++;
-        return i;
     }
 }
