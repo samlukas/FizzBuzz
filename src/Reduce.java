87a72fc2a7a2a66a7b977cfe1aeb7938b34c1f90
samlukas <samuelplukas@gmail.com>
2023-09-11 16:11:24 -0400

    created Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int start = 100;
+
+        while (start != 0) {
+            if (start % 2 == 0) {
+                start /= 2;
+            }
+            else {
+                start -= 1;
+            }
+
+            steps++;
+        }
+
+        System.out.println(steps);
+    }
 }
